b26eed1e4d8f42978ac13c7db5d50db6f6091bb2
RossiniADS <rossini_rodrigues_alvez@hotmail.com>
2020-10-19 08:28:07 -0300

    Ocultando/Mostrando a div (Aulas)

--- Controllers/CursoesController.cs
+++ Controllers/CursoesController.cs
@@ -17,7 +17,11 @@ namespace _30Code.Controllers
         {
             return View(db.Curso.ToList());
         }
-
+        
+        public ActionResult Aula()
+        {
+            return View();
+        }
         // GET: Cursoes
         public ActionResult Index()
         {
