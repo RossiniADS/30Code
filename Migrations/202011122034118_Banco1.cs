161badd077eeab16d95294989a3eb448a9b2ed0a
Rossini <RossiniADS@users.noreply.github.com>
2020-11-12 21:28:32 -0300

    Forms da prova na pagina Aula

---
+++ Migrations/202011122034118_Banco1.cs
@@ -0,0 +1,28 @@
+﻿namespace _30Code.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Banco1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.cur_curso", "Curso_Id", "dbo.cur_curso");
+            DropForeignKey("dbo.con_conteudo", "CursoId", "dbo.cur_curso");
+            DropIndex("dbo.con_conteudo", new[] { "CursoId" });
+            DropIndex("dbo.cur_curso", new[] { "Curso_Id" });
+            DropColumn("dbo.con_conteudo", "CursoId");
+            DropColumn("dbo.cur_curso", "Curso_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.cur_curso", "Curso_Id", c => c.Int());
+            AddColumn("dbo.con_conteudo", "CursoId", c => c.Int(nullable: false));
+            CreateIndex("dbo.cur_curso", "Curso_Id");
+            CreateIndex("dbo.con_conteudo", "CursoId");
+            AddForeignKey("dbo.con_conteudo", "CursoId", "dbo.cur_curso", "cur_id", cascadeDelete: true);
+            AddForeignKey("dbo.cur_curso", "Curso_Id", "dbo.cur_curso", "cur_id");
+        }
+    }
+}
